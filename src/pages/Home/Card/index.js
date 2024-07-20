f4ae0cd607a4b9d8422965aab11c931d2aa4d38d
netoodev <netoodeveloper@gmail.com>
2024-07-20 17:34:41 -0300

    Creating Card component

--- src/pages/Home/Card/index.js
+++ src/pages/Home/Card/index.js
@@ -1,8 +1,31 @@
+import img1 from "img/01.png";
+
 const Card = () => {
-    return (
-      <>
-      </>
-    );
-  };
-  
-  export default Card;
+  return (
+    <>
+      <div className="grid-4 card p-0">
+        <div className="thumb hidden">
+          <a href="#">
+            <img src={img1} alt="" />
+          </a>
+        </div>
+        <div className="mt-2 px-2">
+          <h6 className="color-gray">17 JUL 2024</h6>
+          <h6 className="uppercase color-primary">Tecnologia</h6>
+          <h4>O que esperar do cinema em 2021?</h4>
+          <p className="mt-1">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Exercitationem veritatis sed optio labore.
+          </p>
+          <div className="my-3">
+            <a href="#" className="link color-primary">
+              Ler mais
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,13 +14,11 @@ const Home = () => {
       <section className="container">
         <div className="row">
           <div className="grid-5">
-            <img
-              src={starIcon}
-              className="icon-l"
-              alt="Ícone de Destaque"
-            />
+            <img src={starIcon} className="icon-l" alt="Ícone de Destaque" />
 
-            <h3 className="mt-2">Os melhores e mais bem votados posts do mês.</h3>
+            <h3 className="mt-2">
+              Os melhores e mais bem votados posts do mês.
+            </h3>
             <p className="mt-2">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare
               urna pharetra ut ac, pellentesque. Lorem ipsum dolor sit amet,
@@ -30,12 +28,33 @@ const Home = () => {
           </div>
           <div className="grid-7">
 
-            <Main/>
-            <Main/>
+
+            <Main />
+            <Main />
+
 
           </div>
         </div>
       </section>
+
+
+
+
+
+      <div className="bg-section">
+        <section className="container">
+          <h3 className="ml-2 mb-3">Mais vistos</h3>
+          <div className="row">
+
+
+            <Card/>
+            <Card/>
+            <Card/>
+
+
+          </div>
+        </section>
+      </div>
     </>
   );
 };
