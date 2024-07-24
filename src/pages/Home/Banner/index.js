deb2961225e7ecf97db07775ee191e20af7f1b3d
netoodev <netoodeveloper@gmail.com>
2024-07-24 17:36:26 -0300

    Create and update pages content

--- src/pages/Home/Banner/index.js
+++ src/pages/Home/Banner/index.js
@@ -1,4 +1,4 @@
-import img from "img/02.png";
+import { Link } from "react-router-dom";
 
 const Banner = ({content}) => {
   return (
@@ -11,14 +11,16 @@ const Banner = ({content}) => {
           <h6 className="color-gray text-center">{content.date}</h6>
           <h6 className="uppercase color-primary text-center">{content.category}</h6>
 
-          <h3 className="text-center">{content.title}</h3>
+          <Link to={"/post/" + content.id} className="link-title">
+            <h3 className="text-center">{content.title}</h3>
+          </Link>
           <p className="mt-1 text-center">
           {content.resume}
           </p>
           <div className="my-3 flex-center">
-            <a href="#" className="link color-primary">
+            <Link to={"/post/" + content.id} className="link color-primary">
               Ler mais
-            </a>
+            </Link>
           </div>
         </div>
       </section>
--- src/pages/Home/Card/index.js
+++ src/pages/Home/Card/index.js
@@ -1,24 +1,27 @@
+import { Link } from "react-router-dom";
 
 const Card = ({content}) => {
   return (
     <>
       <div className="grid-4 card p-0">
         <div className="thumb hidden">
-          <a href="#">
+          <Link to={"/post/" + content.id} className="p-0">
             <img src={content.imageUrl} alt="" />
-          </a>
+          </Link>
         </div>
         <div className="mt-2 px-2">
           <h6 className="color-gray">{content.date}</h6>
           <h6 className="uppercase color-primary">{content.category}</h6>
-          <h4>{content.title}</h4>
+          <Link to={"/post/" + content.id} className="link-title">
+            <h4>{content.title}</h4>
+          </Link>
           <p className="mt-1">
           {content.resume}
           </p>
           <div className="my-3">
-            <a href="#" className="link color-primary">
+            <Link to={"/post/" + content.id} className="link color-primary">
               Ler mais
-            </a>
+            </Link>
           </div>
         </div>
       </div>
